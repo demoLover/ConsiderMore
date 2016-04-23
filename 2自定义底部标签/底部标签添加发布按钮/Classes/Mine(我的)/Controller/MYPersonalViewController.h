071301acfe60edf514ee4a1d545b3f8cf2213deb
manager <2283856877@qq.com>
2016-04-23 22:36:48 +0800

    定义了子控制器，添加了导航控制器

---
+++ 2自定义底部标签/底部标签添加发布按钮/Classes/Mine(我的)/Controller/MYPersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYPersonalViewController.h
+//  底部标签添加发布按钮
+//
+//  Created by admin on 16/4/23.
+//  Copyright © 2016年 程涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYPersonalViewController : UITableViewController
+
+@end
--- 2自定义底部标签/底部标签添加发布按钮/MYViewController.m
+++ 2自定义底部标签/底部标签添加发布按钮/MYViewController.m
@@ -9,6 +9,13 @@
 #import "MYViewController.h"
 #import "MYTabBar.h"
 
+#import "MYEssenceViewController.h"
+#import "MYNewViewController.h"
+#import "MYFoucsViewController.h"
+#import "MYPersonalViewController.h"
+
+#import "MYNavViewController.h"
+
 
 #define MYScreenW [UIScreen mainScreen].bounds.size.width
 
@@ -55,23 +62,22 @@
 {
     //精华
     
-    UIViewController *vc1 = [[UIViewController alloc] init];
-    vc1.view.backgroundColor = [UIColor redColor];
+    MYEssenceViewController *vc1 = [[MYEssenceViewController alloc] init];
+
     [self setupOneChildController:vc1 title:@"精华" image:[UIImage imageNamed:@"tabBar_essence_icon"] selImage:[UIImage imageNamed:@"tabBar_essence_click_icon"]];
     
     //新帖
-    UIViewController *vc2 = [[UIViewController alloc] init];
-    vc2.view.backgroundColor = [UIColor grayColor];
+    MYNewViewController *vc2 = [[MYNewViewController alloc] init];
+
     [self setupOneChildController:vc2 title:@"新帖" image:[UIImage imageNamed:@"tabBar_new_icon"] selImage:[UIImage imageNamed:@"tabBar_new_click_icon"]];
     
     //关注
-    UIViewController *vc4 = [[UIViewController alloc] init];
-    vc4.view.backgroundColor = [UIColor blueColor];
+    MYFoucsViewController *vc4 = [[MYFoucsViewController alloc] init];
+
     [self setupOneChildController:vc4 title:@"关注" image:[UIImage imageNamed:@"tabBar_friendTrends_icon"] selImage:[UIImage imageNamed:@"tabBar_friendTrends_click_icon"]];
     
     //我的
-    UIViewController *vc5 = [[UIViewController alloc] init];
-    vc5.view.backgroundColor = [UIColor yellowColor];
+    MYPersonalViewController *vc5 = [[MYPersonalViewController alloc] init];
     [self setupOneChildController:vc5 title:@"我的" image:[UIImage imageNamed:@"tabBar_me_icon"] selImage:[UIImage imageNamed:@"tabBar_me_click_icon"]];
     
 
@@ -84,14 +90,15 @@
 - (void)setupOneChildController:(UIViewController *)viewController title:(NSString *)title image:(UIImage *)image selImage:(UIImage *)selImage
 {
     
-    [self addChildViewController:viewController];
+    MYNavViewController *nav = [[MYNavViewController alloc] initWithRootViewController:viewController];
+    [self addChildViewController:nav];
+
+    viewController.tabBarItem.title = title;
+    viewController.tabBarItem.image = image;
+    viewController.tabBarItem.selectedImage = selImage;
     
-    if (image) {
-        
-        viewController.tabBarItem.title = title;
-        viewController.tabBarItem.image = image;
-        viewController.tabBarItem.selectedImage = selImage;
-    }
+    viewController.navigationItem.title = title;
+    
 }
 
 
